db96f32de8c09d7e0722f3d78e2b64061ff0e99c
kerembilim <kerembilim@gmail.com>
2023-02-12 23:15:07 +0300

    [FIX]

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddSwaggerGen(c => {
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -11,6 +12,11 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else{
+    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
